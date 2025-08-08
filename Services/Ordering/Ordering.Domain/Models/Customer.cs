9277ef5702af738adcdebd051223d32701587827
Guilherme.fadel <guilherme.fadel@becomex.com.br>
2025-08-08 09:54:21 -0300

    ordering domain

---
+++ Services/Ordering/Ordering.Domain/Models/Customer.cs
@@ -0,0 +1,21 @@
+﻿using Ordering.Domain.Abstractions;
+
+namespace Ordering.Domain.Models;
+public class Customer : Entity<CustomerId>
+{
+    public string Name { get; private set; } = null!;
+    public string Email { get; private set; } = null!;
+
+    public static Customer Create(CustomerId id, string name, string email)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(name);
+        ArgumentException.ThrowIfNullOrWhiteSpace(email);
+
+        return new Customer
+        {
+            Id = id,
+            Name = name,
+            Email = email
+        };
+    }
+}
