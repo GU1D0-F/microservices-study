2be96701876cfdcb256e7775d906fc6a5e41854e
Guilherme.fadel <guilherme.fadel@becomex.com.br>
2025-08-13 10:12:22 -0300

    Ordering Application + BuildingBlocks

--- Services/Ordering/Ordering.Application/Dtos/AddressDto.cs
+++ Services/Ordering/Ordering.Application/Dtos/AddressDto.cs
@@ -1,2 +1,10 @@
 ﻿namespace Ordering.Application.Dtos;
-public record AddressDto(string FirstName, string LastName, string EmailAddress, string AddressLine, string Country, string State, string ZipCode);
+
+public record AddressDto(
+    string FirstName, 
+    string LastName, 
+    string EmailAddress, 
+    string AddressLine, 
+    string Country, 
+    string State, 
+    string ZipCode);
--- Services/Ordering/Ordering.Application/Dtos/OrderItemDto.cs
+++ Services/Ordering/Ordering.Application/Dtos/OrderItemDto.cs
@@ -1,3 +1,7 @@
 ﻿namespace Ordering.Application.Dtos;
 
-public record OrderItemDto(Guid OrderId, Guid ProductId, int Quantity, decimal Price);
+public record OrderItemDto(
+    Guid OrderId, 
+    Guid ProductId, 
+    int Quantity, 
+    decimal Price);
